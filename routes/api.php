4fc54b42ded2ca231304927646cf0b97b5fdba4a
Andi Fajar <andifajarlah@gmail.com>
2022-11-16 14:57:39 +0800

    baru

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,5 @@ Route::middleware(['cors'])->get('/targetbaca/{pembaca}', function ($pembaca) {
   $pembaca = \App\Models\Pembaca::where('uid', $pembaca)->withoutGlobalScopes()->first();
   return \App\Models\BacaMeter::withoutGlobalScopes()->where('pengguna_id', $pembaca->pengguna_id)->get();
 });
+
+Route::middleware(['cors'])->post('/update/{pembaca}', 'BacameterController@update');

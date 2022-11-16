7b48091f2b0d156cb4535f53d95437a8cfd809ca
Andi Fajar <andifajarlah@gmail.com>
2022-11-16 14:59:11 +0800

    baru

---
+++ app/Http/Controllers/BacameterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BacameterController extends Controller
+{
+  //
+  public function update(Request $req)
+  {
+    // $req->validate([
+    //     'id'
+    // ])
+  }
+}

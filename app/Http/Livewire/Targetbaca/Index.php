e165f6b4ab0a6c8896dd8f4a9b9990ace6e9e9e6
Andi Fajar <andifajarlah@gmail.com>
2022-12-26 11:11:29 +0800

    bru

--- app/Http/Livewire/Targetbaca/Index.php
+++ app/Http/Livewire/Targetbaca/Index.php
@@ -11,52 +11,56 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class Index extends Component
 {
-  use WithPagination;
+    use WithPagination;
 
-  public $bulan, $tahun, $status = 0, $statusBaca, $pembaca, $cari, $hapus;
-  protected $paginationTheme = 'bootstrap';
+    public $bulan, $tahun, $status = 0, $statusBaca, $pembaca, $cari, $hapus;
+    protected $paginationTheme = 'bootstrap';
 
-  protected $queryString = ['bulan', 'tahun', 'status', 'pembaca', 'cari', 'statusBaca'];
+    protected $queryString = ['bulan', 'tahun', 'status', 'pembaca', 'cari', 'statusBaca'];
 
-  public function mount()
-  {
-    $this->bulan = $this->bulan ?: date('m');
-    $this->tahun = $this->tahun ?: date('Y');
-  }
-
-  public function setHapus($hapus = null)
-  {
-    $this->hapus = $hapus;
-  }
-
-  public function hapus()
-  {
-    $data = BacaMeter::findOrFail($this->hapus);
-    File::delete($data->foto);
-    $data->delete();
-    $this->hapus = null;
-  }
-
-  public function updated()
-  {
-    $this->resetPage();
-  }
-
-  public function export()
-  {
-    $this->emit('reinitialize');
-    return Excel::download(new BacameterExport($this->cari, $this->status, $this->statusBaca, $this->pembaca, $this->tahun, $this->bulan), 'Data Baca Meter - ' . $this->tahun . $this->bulan . '.xlsx');
-  }
-
-  public function render()
-  {
-    if ($this->status == 0) {
-      $this->statusBaca = null;
+    public function mount()
+    {
+        $this->bulan = $this->bulan ?: date('m');
+        $this->tahun = $this->tahun ?: date('Y');
     }
-    $data = BacaMeter::where(fn($q) => $q->where('no_langganan', 'like', '%' . $this->cari . '%')->orWhere('nama', 'like', '%' . $this->cari . '%'))->when($this->status == 1, fn($q) => $q->whereNotNull('tanggal_baca'))->when($this->status == 0, fn($q) => $q->whereNull('tanggal_baca'))->when($this->statusBaca, fn($q) => $q->where('status_baca', $this->statusBaca))->when($this->pembaca, fn($q) => $q->where('pembaca_kode', $this->pembaca))->where('periode', $this->tahun . '-' . $this->bulan . '-01')->paginate(10);
-    return view('livewire.targetbaca.index', [
-      'no' => ($this->page - 1) * 10,
-      'data' => $data,
-    ]);
-  }
+
+    public function setHapus($hapus = null)
+    {
+        $this->hapus = $hapus;
+    }
+
+    public function hapus()
+    {
+        $data = BacaMeter::findOrFail($this->hapus);
+        File::delete($data->foto);
+        $data->status_baca = null;
+        $data->stand_ini = null;
+        $data->tanggal_baca = null;
+        $data->foto = null;
+        $data->save();
+        $this->hapus = null;
+    }
+
+    public function updated()
+    {
+        $this->resetPage();
+    }
+
+    public function export()
+    {
+        $this->emit('reinitialize');
+        return Excel::download(new BacameterExport($this->cari, $this->status, $this->statusBaca, $this->pembaca, $this->tahun, $this->bulan), 'Data Baca Meter - ' . $this->tahun . $this->bulan . '.xlsx');
+    }
+
+    public function render()
+    {
+        if ($this->status == 0) {
+            $this->statusBaca = null;
+        }
+        $data = BacaMeter::where(fn($q) => $q->where('no_langganan', 'like', '%' . $this->cari . '%')->orWhere('nama', 'like', '%' . $this->cari . '%'))->when($this->status == 1, fn($q) => $q->whereNotNull('tanggal_baca'))->when($this->status == 0, fn($q) => $q->whereNull('tanggal_baca'))->when($this->statusBaca, fn($q) => $q->where('status_baca', $this->statusBaca))->when($this->pembaca, fn($q) => $q->where('pembaca_kode', $this->pembaca))->where('periode', $this->tahun . '-' . $this->bulan . '-01')->paginate(10);
+        return view('livewire.targetbaca.index', [
+            'no' => ($this->page - 1) * 10,
+            'data' => $data,
+        ]);
+    }
 }

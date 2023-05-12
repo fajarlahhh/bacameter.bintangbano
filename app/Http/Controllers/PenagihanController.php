fe5210d887c19959dd3b9165f8f209dd0b1b8d09
Andi Fajar <andifajarlah@gmail.com>
2023-05-12 19:31:00 +0800

    baru

--- app/Http/Controllers/PenagihanController.php
+++ app/Http/Controllers/PenagihanController.php
@@ -31,7 +31,7 @@ class PenagihanController extends Controller
                 if ($pengguna->cabang_id) {
                     return response()->json([
                         'status' => 'sukses',
-                        'data' => Tagihan::when($pengguna->cabang_id, fn ($q) => $q->where('cabang_id', $pengguna->cabang_id))->where(fn ($q) => $q->where('nama', 'like', '%' . $req->cari . '%')->orWhere('no_langganan', 'like', '%' . $req->cari . '%'))->groupBy('no_langganan')->select('no_langganan', 'nama', 'alamat', 'cabang_id', 'golongan', 'status')->whereNull('tanggal_tagih')->with('tagihan')->get(),
+                        'data' => Tagihan::when($pengguna->cabang_id, fn ($q) => $q->where('cabang_id', $pengguna->cabang_id))->where(fn ($q) => $q->where('nama', 'like', '%' . $req->cari . '%')->orWhere('no_langganan', 'like', '%' . $req->cari . '%'))->groupBy('no_langganan')->select('no_langganan', 'nama', 'alamat', 'cabang_id')->whereNull('tanggal_tagih')->with('tagihan')->get(),
                     ]);
                 } else {
                     return response()->json([
--- app/Models/Tagihan.php
+++ app/Models/Tagihan.php
@@ -33,7 +33,7 @@ class Tagihan extends Model
 
     public function tagihan()
     {
-        return $this->hasMany(Tagihan::class, 'no_langganan', 'no_langganan')->select('stand_lalu', 'stand_ini', DB::raw('stand_ini-stand_lalu pakai'), 'no_langganan', 'jumlah', 'periode', DB::raw('if( date(  NOW()) > DATE_ADD( STR_TO_DATE(concat( SUBSTR( periode, 1, 8 ), "25" ), "%Y-%m-%d"), INTERVAL 1 MONTH), 5000, 0 ) denda'), 'id')->whereNull('tanggal_tagih')->orderBy('periode', 'desc');
+        return $this->hasMany(Tagihan::class, 'no_langganan', 'no_langganan')->select('golongan', 'status', 'stand_lalu', 'stand_ini', DB::raw('stand_ini-stand_lalu pakai'), 'no_langganan', 'jumlah', 'periode', DB::raw('if( date(  NOW()) > DATE_ADD( STR_TO_DATE(concat( SUBSTR( periode, 1, 8 ), "25" ), "%Y-%m-%d"), INTERVAL 1 MONTH), 5000, 0 ) denda'), 'id')->whereNull('tanggal_tagih')->orderBy('periode', 'desc');
     }
 
     public function cabang()
